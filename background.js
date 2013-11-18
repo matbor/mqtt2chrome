6169d46546242512793b1218870ba3b008dfc089
matbor <matthew@bordignons.net>
2013-11-18 15:37:10 +1100

    fixed the connect/disconnect

--- background.js
+++ background.js
@@ -72,7 +72,12 @@ if (window.webkitNotifications) {
 //when you click on the icon on the extension bar it will disconnect/reconnect
 chrome.browserAction.onClicked.addListener(function () {
   console.log('we will try to disconnect and reconnect');
-  client.disconnect();
+  try{
+        client.disconnect();
+        }
+  catch (e) {
+        console.log(e);
+        };
   connect();
 });
 
